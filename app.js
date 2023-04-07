7f260d9c060b25dac2ff2d48ed1c862d8a63b33f
snehamoybag <snehamoybag2@gmail.com>
2023-04-07 19:38:50 +0530

    add attribution links

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
 const url = 'https://api.adviceslip.com/advice';
 
-const renderAdviceId = (dataSlip) => {
-  const test = document.querySelector('#advice-id');
-  test.textContent = dataSlip.id;
-}
+const renderTxtContent = (elId, text) => {
+  const el = document.getElementById(elId);
+  el.textContent = text;
+};
 
-const renderAdviceQuote = (dataSlip) => {
-  const test = document.querySelector('#advice-quote');
-  test.textContent = dataSlip.advice;
-}
+const renderAdvice = async () => {
+  // show loading texts by default
+  renderTxtContent('advice-id', 'loading...');
+  renderTxtContent('advice-quote', 'Please wait a moment...');
 
-const renderAdviceEls = (dataSlip) => {
-  renderAdviceId(dataSlip);
-  renderAdviceQuote(dataSlip);
-}
-fetch(url)
-.then(response => response.json())
-.then(data => data.slip)
-.then(renderAdviceEls)
-.catch((err) => console.log(err));
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    // show error if response status is not ok
+    if (!response.ok) {
+      renderTxtContent('advice-id', 'error');
+      renderTxtContent('advice-quote', 'Something went wrong! Please try again later.');
+    } else {
+      renderTxtContent('advice-id', data.slip.id);
+      renderTxtContent('advice-quote', data.slip.advice);
+    }
+  }
+  catch (error) {
+    const errMsg = error.message + '. Please try again later.'
+    renderTxtContent('advice-quote', errMsg);
+    renderTxtContent('advice-id', 'error');
+  }
+};
+// render a advice on page load;
+renderAdvice()
+
+// render new advice on button click
+document.querySelector('#btn-advice').addEventListener('click', renderAdvice);
